714a4373ff8ef90b8656edb8ea72eda290f48425
Sebastian Kann <sebastiankann@gmail.com>
2020-09-14 20:19:15 +0000

    added head function

---
+++ head.js
@@ -0,0 +1,18 @@
+//TEST FUNCTION
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`🌈🌈 Assertion Passed: ${actual} === ${expected} 🌈🌈`);
+  } else {
+    console.log(`🚧🚧 Assertion Failed: ${actual} !== ${expected} 🚧🚧`);
+  }
+};
+
+//HEAD FUNCTION
+ const head = function(arr){
+   return arr[0];
+ }
+
+
+
+assertEqual(head([5]), 5);
+assertEqual(head([]), "Hello");
