6d1a6cb20fbd191bd1d81cb346f8c1f93dc46fa4
Sebastian Kann <sebastiankann@gmail.com>
2020-09-14 19:59:01 +0000

    wrote assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🌈🌈 Assertion Passed: ${actual} === ${expected} 🌈🌈`);
+  } else {
+    console.log(`🚧🚧 Assertion Failed: ${actual} !== ${expected} 🚧🚧`);
+  }
+};
+
+// TEST CODE
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1, 300);
