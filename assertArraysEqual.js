2a57f2d18e075910897d4dd7e92aa3156525ca37
Sebastian Kann <sebastiankann@gmail.com>
2020-09-16 02:20:35 +0000

    created assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,37 @@
+// This function compares arrays and returns a boolean value if they are equivalent
+const eqArrays = function (arr1, arr2) {
+  // Define a boolean value as true
+  let equality = true;
+  // declare an index counter
+  let index = -1
+  // as long as the index is less than the length of the array and the boolean remains true ...
+  while (index < arr1.length && equality) {
+    // increment the index
+    index++
+    // then see if the elements at the index are not the same in both arrays. If they are not...
+    if (arr1[index] !== arr2[index]) {
+      equality = false;
+    }
+  }
+  return equality;
+}
+
+//TEST FUNCTION
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`🌈🌈 Assertion Passed: ${actual} === ${expected} 🌈🌈`);
+  } else {
+    console.log(`🚧🚧 Assertion Failed: ${actual} !== ${expected} 🚧🚧`);
+  }
+};
+
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+
+
+
+
