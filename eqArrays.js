9c707b289e611bec6209b9464b0f7a041e149211
Sebastian Kann <sebastiankann@gmail.com>
2020-09-16 00:03:57 +0000

    created eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -33,10 +33,6 @@ const eqArrays = function (arr1, arr2) {
 
 
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 4]), false);
 
 
 
