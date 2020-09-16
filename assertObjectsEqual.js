2a899f3f1cd5c9e95cc5a3c871f7ace4bdd1ead8
Sebastian Kann <sebastiankann@gmail.com>
2020-09-16 22:36:36 +0000

    wrote assertObjectsEqual.js

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,23 +1,17 @@
-
-
-
-
 // this function compares two objects to see if they have the same number of keys and returns 'true' if they do
-const sameNumberOfKeys = function(obj1, obj2) {
+const sameNumberOfKeys = function (obj1, obj2) {
   // declare a variable equal to the array of keys of object 1
   const keyChain1 = Object.keys(obj1);
   // declare a variable equal to the array of keys of object 2
   const keyChain2 = Object.keys(obj2);
   // if the length of arr1 !== length arr2
   if (keyChain1.length !== keyChain2.length) {
-    return false
-  } else
-  return true;
-}
+    return false;
+  } else return true;
+};
 
 // this function compares two objects and if the value for each key in one object is the same as the value for that same key in the other object, the function returns true
-const compareValues = function(obj1, obj2) {
-  
+const compareValues = function (obj1, obj2) {
   // for each key in object 1
   for (const key in obj1) {
     //if object1.key is an array
@@ -28,7 +22,7 @@ const compareValues = function(obj1, obj2) {
     }
     // if object1.key !== object2.key, return false
     else if (obj1[key] !== obj2[key]) {
-      return false
+      return false;
     }
   }
   // for each key in object 2
@@ -46,25 +40,25 @@ const compareValues = function(obj1, obj2) {
   }
   // otherwise, return true
   return true;
-}
+};
 
 // This function compares arrays and returns a boolean value if they are equivalent
 const eqArrays = function (arr1, arr2) {
   // Define a boolean value as true
   let equality = true;
   // declare an index counter
-  let index = -1
+  let index = -1;
   // as long as the index is less than the length of the array and the boolean remains true ...
   while (index < arr1.length && equality) {
     // increment the index
-    index++
+    index++;
     // then see if the elements at the index are not the same in both arrays. If they are not...
     if (arr1[index] !== arr2[index]) {
       equality = false;
     }
   }
   return equality;
-}
+};
 
 const eqObjects = function (actual, expected) {
   // Do these objects have the same number of keys?
@@ -78,9 +72,21 @@ const eqObjects = function (actual, expected) {
   }
 };
 
+// this function compares two simple objects. If they have the same number of keys and the value for each key in one object is the same as the value for that same key in the other object, the function returns 'true
+const assertObjectsEqual = function (actual, expected) {
+  //import inspect
+  const inspect = require('util').inspect;
+  // Do these objects have the same number of keys?
+  if (!eqObjects(actual, expected)) {
+    console.log(`🚧🚧 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)} 🚧🚧`);
+  } else {
+    console.log(`🌈🌈 Assertion Passed: ${inspect(actual)} === ${inspect(expected)} 🌈🌈`);
+  }
+};
+
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
-console.log(eqObjects(cd, dc)); // => true
+assertObjectsEqual(cd, dc); // => true
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(eqObjects(cd, cd2)); // => false
+assertObjectsEqual(cd, cd2); // => false
