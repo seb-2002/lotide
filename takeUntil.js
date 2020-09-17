8f7501fd0ab7547f37c966202582597486e27994
Sebastian Kann <sebastiankann@gmail.com>
2020-09-17 19:05:00 +0000

    wrote takeUntil.js

---
+++ takeUntil.js
@@ -0,0 +1,36 @@
+const {assertArraysEqual} = require('./assertArraysEqual');
+
+const takeUntil = function(array, callback) {
+  const result = [];
+  for (let elm of array) {
+    if (callback(elm)) {
+      break;
+    } else {
+      result.push(elm);
+    }
+  }
+  return result
+};
+ 
+
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, (x) => x < 0);
+console.log(results1);
+
+console.log("---");
+
+const data2 = [
+  "I've",
+  "been",
+  "to",
+  "Hollywood",
+  ",",
+  "I've",
+  "been",
+  "to",
+  "Redwood",
+];
+const results2 = takeUntil(data2, (x) => x === ",");
+console.log(results2);
