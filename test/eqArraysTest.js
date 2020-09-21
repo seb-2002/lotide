80193fe8a887e7764b721f3fa15537823799e740
Sebastian Kann <sebastiankann@gmail.com>
2020-09-21 20:25:04 +0000

    updated .gitignore

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,7 +1,15 @@
 const eqArrays = require('../eqArrays');
+const assert = require("chai").assert;
 
+describe('#eqArrays', () => {
+  let myArray = [1, 2, 3, 4];
+  let my2ndArray = [1, 2, 3, 5];
+  it('returns true when given two equal arrays', () => {
+
+  })
+})
 
 let myArray = [1, 2, 3, 4];
-let my2ndArray = [1, 2, 3, 4];
+let my2ndArray = [1, 2, 3, 5];
 
 console.log(eqArrays(myArray, my2ndArray));
