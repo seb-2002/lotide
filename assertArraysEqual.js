509bda3429d886e2addb685adf75a2d153ee1248
Sebastian Kann <sebastiankann@gmail.com>
2020-09-21 19:39:49 +0000

    refactored assertArraysEqual.js and added a testing file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,20 +1,4 @@
-// This function compares arrays and returns a boolean value if they are equivalent
-const eqArrays = function (arr1, arr2) {
-  // Define a boolean value as true
-  let equality = true;
-  // declare an index counter
-  let index = -1
-  // as long as the index is less than the length of the array and the boolean remains true ...
-  while (index < arr1.length && equality) {
-    // increment the index
-    index++
-    // then see if the elements at the index are not the same in both arrays. If they are not...
-    if (arr1[index] !== arr2[index]) {
-      equality = false;
-    }
-  }
-  return equality;
-}
+const eqArrays = require('./eqArrays');
 
 //TEST FUNCTION
 const assertArraysEqual = function(actual, expected) {
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,7 @@
+const assertArraysEq = require('../assertArraysEqual');
+
+let a = ['bear', 156, ':)', 'hello', 5678, -2];
+let b = ['whattttttt', 666666]
+
+assertArraysEq(a, a);
+assertArraysEq(a, b);
