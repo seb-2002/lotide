1eb9e70e75f61ac6540551bcd73829cb54855530
Sebastian Kann <sebastiankann@gmail.com>
2020-09-21 20:39:26 +0000

    refactored eqArraysTest to use mocha

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -2,14 +2,14 @@ const eqArrays = require('../eqArrays');
 const assert = require("chai").assert;
 
 describe('#eqArrays', () => {
-  let myArray = [1, 2, 3, 4];
-  let my2ndArray = [1, 2, 3, 5];
   it('returns true when given two equal arrays', () => {
-
-  })
-})
+    assert.isTrue(eqArrays(myArray, myArray));
+  });
+  it('returns false when passed two unequal arrays', () => {
+    assert.isFalse(eqArrays(myArray, my2ndArray));
+  });
+});
 
 let myArray = [1, 2, 3, 4];
 let my2ndArray = [1, 2, 3, 5];
 
-console.log(eqArrays(myArray, my2ndArray));
