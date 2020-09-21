86609441af797a9500c692520ecd955196cd8189
Sebastian Kann <sebastiankann@gmail.com>
2020-09-21 20:04:47 +0000

    refactored headTest.js in with mocha

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,12 @@
 const head = require('../head')
-const assertEqual = require('../assertEqual')
+const assert = require('chai').assert;
 
-assertEqual(head([5]), 5);
-assertEqual(head([]), "Hello");
+
+describe('#head', () => {
+  it('returns 5 for [5, 6, 7]', () => {
+    assert.strictEqual(head([5,6,7]), 5);
+  });
+  it('returns 5 for [5]', () => {
+    assert.strictEqual(head([5]), 5);
+  });
+});
